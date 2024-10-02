3eedb4eb93a1df01af98b9925d1bbfbcec0b8ace
Carlos Cassol <carlos.cassol@martinellilabs.com.br>
2024-10-02 20:00:56 -0300

    Adicionado construtor para bolsas.

--- microservfinanceiro/Financeiro/Entities/Bolsas.cs
+++ microservfinanceiro/Financeiro/Entities/Bolsas.cs
@@ -11,5 +11,18 @@ namespace microservfinanceiro.Financeiro.Entities
         public DateTime DataFimVigencia { get; set; }
         public DateTime DataAplicacao { get; set; }
         public bool Disponibilidade { get; set; }
+
+        public Bolsas() { }
+
+        public Bolsas(Guid id, TipoBolsasEnum tipo, decimal porcentagemDesconto, DateTime dataInicioVigencia, DateTime dataFimVigencia, DateTime dataAplicacao, bool disponibilidade)
+        {
+            Id = id;
+            Tipo = tipo;
+            PorcentagemDesconto = porcentagemDesconto;
+            DataInicioVigencia = dataInicioVigencia;
+            DataFimVigencia = dataFimVigencia;
+            DataAplicacao = dataAplicacao;
+            Disponibilidade = disponibilidade;
+        }
     }
 }
