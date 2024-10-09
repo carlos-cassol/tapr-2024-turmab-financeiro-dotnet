c9763c090e3259a2dd6af9b0f396e1ce0bcb1479
MarcosStinglin <102559239+MarcosStinglin@users.noreply.github.com>
2024-10-09 00:11:05 +0000

    Criado Classe

---
+++ microservfinanceiro/Financeiro/Entities/Emissoes.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using microservfinanceiro.Financeiro.Enumerables;
+
+namespace microservfinanceiro.Financeiro.Entities
+{
+    public class Emissoes
+    {
+        public Guid Id { get; set; }
+        public decimal Valor { get; set; }
+
+        public string VencimentoPagamento { get; set; }
+
+        public string TipoPagamento { get; set; }
+
+        public decimal Juros { get; set; }
+
+        public TipoBolsasEnum Tipo { get; set; }
+
+        public decimal Porcentagem { get; set; }
+
+        public Emissoes () {}
+
+        public Emissoes (Guid id, decimal valor, string vencimentopagamento, string tipopagamento, decimal juros, TipoBolsasEnum tipo, decimal porcentagem)
+        {
+            Id = id;
+            Valor = valor;
+            VencimentoPagamento = vencimentopagamento;
+            TipoPagamento = tipopagamento;
+            Juros = juros;
+            Tipo = tipo;
+            Porcentagem = porcentagem;
+        }
+    }
+}
