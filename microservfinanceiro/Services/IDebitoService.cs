a03e6c9b9faf6914433e489fb7f7beb8b9425c66
MarcosStinglin <102559239+MarcosStinglin@users.noreply.github.com>
2024-10-30 22:53:39 +0000

    DebitoController

--- microservfinanceiro/Services/IDebitoService.cs
+++ microservfinanceiro/Services/IDebitoService.cs
@@ -2,12 +2,15 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using microservfinanceiro.Financeiro.Entities;
 
 namespace Services
 {
     public interface IDebitoService
     {
-        public List<Debito> GetAll();
-        public Debito Get();
+        public List<Debitos> GetAll();
+        public Debitos Get();
+        Task<object?> GetAllAsync();
+        Task<object?> SaveAsync(Debitos aluno);
     }
 }
